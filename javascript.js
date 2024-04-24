ab5099dfabfef8579445c61e4b22c565c274ef21
Kenny Sorensen <kenny.d.sorensen@gmail.com>
2024-04-24 13:30:58 -0400

    added button to determine grid-size

--- javascript.js
+++ javascript.js
@@ -1,21 +1,41 @@
 const container = document.querySelector(".container");
-
-for(a = 0; a < 16; a++)
+const button = document.querySelector("button");
+let gridSize = 16;
+createGrid(gridSize);
+button.onclick = function()
 {
-    const squareRow = document.createElement("div");
-    squareRow.className = "squareRow";
-
-    for(b = 0; b < 16; b++)
+    gridSize = prompt("Enter the number of squares you would like per side to form the grid: ");
+    while(gridSize > 100)
     {
-        const square = document.createElement("div");
-        square.className = "square";
-
-        square.onmouseover = function()
-        {
-            square.style.backgroundColor = "red";
-        };
-        
-        squareRow.appendChild(square);
+        gridSize = prompt("Max grid length is 100 squares. Please enter a number less than 100.\n\nEnter the number of squares you would like per side to form the grid: ");
     }
-    container.appendChild(squareRow);
+    while(container.firstChild)
+    {
+        container.removeChild(container.firstChild);
+    }
+    createGrid(gridSize);
+};
+
+function createGrid(gridSize)
+{
+    for(a = 0; a < gridSize; a++)
+    {
+        const squareRow = document.createElement("div");
+        squareRow.className = "squareRow";
+
+        for(b = 0; b < gridSize; b++)
+        {
+            const square = document.createElement("div");
+            square.className = "square";
+
+            square.onmouseover = function()
+            {
+                square.style.backgroundColor = "red";
+            };
+            
+            squareRow.appendChild(square);
+        }
+        container.appendChild(squareRow);
+    }
 }
+
