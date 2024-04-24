0423987948f8cf4262785e679e607b6fc321088c
Kenny Sorensen <kenny.d.sorensen@gmail.com>
2024-04-24 00:11:05 -0400

    created a 16x16 grid that fills the pg in flex

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+
+for(a = 0; a < 16; a++)
+{
+    const squareRow = document.createElement("div");
+    squareRow.className = "squareRow";
+
+    for(b = 0; b < 16; b++)
+    {
+        const square = document.createElement("div");
+        square.className = "square";
+        squareRow.appendChild(square);
+    }
+    container.appendChild(squareRow);
+}
